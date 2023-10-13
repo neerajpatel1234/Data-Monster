e43aa81b00981f50e4eb5ee14b39cf9111c4dcb3
Neeraj Patel <neerajnpatel18@gmail.com>
2023-10-13 21:27:48 +1300

    inti commit with foundation code to parse the data

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import pandas as pd
 
+def excel_to_tables(file_path):
+    xls = pd.ExcelFile(file_path)
+    tables = {}
+    
+    for sheet_name in xls.sheet_names:
+        df = pd.read_excel(xls, sheet_name)
+        tables[sheet_name] = df
+    
+    return tables
+
+
+# ----------- Variables -----------
+file_path = 'example.xlsx'  # Replace with your Excel file path
+tables = excel_to_tables(file_path)
+
+tables['Sheet_Title']
