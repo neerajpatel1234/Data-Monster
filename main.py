69d75673e46cfa82b473281cfb1b0447f36716e6
Neeraj Patel <neerajnpatel18@gmail.com>
2023-10-18 20:08:56 +1300

    new gui

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+import tkinter as tk
+from tkinter import filedialog
 import pandas as pd
 
-# ----------- Test Pandas -----------
-testData = pd.Series([1,2,3])
-print(testData)
-
-# ----------- Function Defs -----------
+# ----------- Function Definitions -----------
 def excel_to_tables(file_path):
     try:
         xls = pd.ExcelFile(file_path)
@@ -24,25 +22,59 @@ def excel_to_tables(file_path):
     
     return tables
 
-# ----------- Ask for file name -----------
-file_path = input('Enter file path: ')  
-if not file_path:
-    print('ERROR: No file path entered.')
-    exit()
-    
-# ----------- Read Excel file -----------
-tables = excel_to_tables(file_path)
+def save_to_csv(dataframe, file_name):
+    try:
+        dataframe.to_csv(file_name, index=False)
+        print(f'Successfully saved to {file_name}')
+    except Exception as e:
+        print(f'ERROR: {e}')
 
-if tables:
-    # ----------- Print all sheet names -----------
-    for sheet_name in tables.keys():
-        print(sheet_name)
-        
-    # ----------- Access a specific sheet -----------
-    sheet_name = input('Enter sheet name: ')
-    current_sheet = tables.get(sheet_name)
+def browse_file():
+    global file_path
+    file_path = filedialog.askopenfilename(filetypes=[("Excel Files", "*.xlsx")])
+    if file_path:
+        entry_path.delete(0, tk.END)
+        entry_path.insert(0, file_path)
 
-    if current_sheet is not None:
-        print(current_sheet)
-    else:
-        print(f'ERROR: Sheet "{sheet_name}" not found.')
+def convert_to_csv():
+    file_path = entry_path.get()
+    tables = excel_to_tables(file_path)
+    if tables:
+        for sheet_name in tables.keys():
+            listbox_sheets.insert(tk.END, sheet_name)
+
+def save_csv():
+    selected_sheet = listbox_sheets.get(tk.ACTIVE)
+    if selected_sheet:
+        current_sheet = tables[selected_sheet]
+        csv_file_name = filedialog.asksaveasfilename(defaultextension=".csv", filetypes=[("CSV Files", "*.csv")])
+        if csv_file_name:
+            save_to_csv(current_sheet, csv_file_name)
+
+# ----------- GUI -----------
+root = tk.Tk()
+root.title("Excel to CSV Converter")
+
+frame = tk.Frame(root)
+frame.pack(pady=10)
+
+label_path = tk.Label(frame, text="File Path:")
+label_path.grid(row=0, column=0)
+
+entry_path = tk.Entry(frame, width=50)
+entry_path.grid(row=0, column=1, padx=10)
+
+button_browse = tk.Button(frame, text="Browse", command=browse_file)
+button_browse.grid(row=0, column=2)
+
+button_convert = tk.Button(frame, text="Convert", command=convert_to_csv)
+button_convert.grid(row=1, columnspan=3, pady=10)
+
+listbox_sheets = tk.Listbox(root, selectmode=tk.SINGLE, width=50)
+listbox_sheets.pack(pady=10)
+
+button_save = tk.Button(root, text="Save as CSV", command=save_csv)
+button_save.pack()
+
+root.mainloop()
+
